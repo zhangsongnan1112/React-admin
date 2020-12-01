beaa2594191145947c266a0836c1b67dc680f80b
zhangsongnan <zsn19951112@126.com>
2020-12-01 11:30:00 +0800

    解决报错

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import{Button} from 'antd'
-import '../../styles/layout.scss'
+import '../../styles/base.css'
 class Layout extends Component {
   constructor(props) {
     super(props);
